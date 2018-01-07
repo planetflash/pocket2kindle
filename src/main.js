789ed5072c730c274ab35a33d61b2610eafa92be
Paul Thomas <paul@craftedbygc.com>
2018-01-07 21:30:09 +0000

    Revert "test change"
    This reverts commit 06ffa90273a2d45f5d07645fa9ce5621d8350f3e.

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ const hello = document.getElementById('app');
 hello.innerHTML = '<p>Hello 22!</p>';
 
 hello.addEventListener('click', e => {
-	console.log('test change');
 
 	fetch('/api/hello')
     .then(resp => resp.json())
