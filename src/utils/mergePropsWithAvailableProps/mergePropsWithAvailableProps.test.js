224a7e85e1ac572781275991ec201feef82ba036
Paul Thomas <paul@craftedbygc.com>
2018-02-18 17:20:13 +0000

    test for mergePropsWithAvailableProps

---
+++ src/utils/mergePropsWithAvailableProps/mergePropsWithAvailableProps.test.js
@@ -0,0 +1,23 @@
+import mergePropsWithAvailableProps from './index.js';
+
+const props = {
+	validProp1: 'validProp',
+	validProp2: null,
+	validProp3: undefined,
+	invalidProp: 'anotherInvalidProp',
+};
+
+const allowedProps = {
+	validProp1: null,
+	validProp2: null,
+	validProp3: null,
+}
+
+test('valid props to be merged', () => {
+	const expected = {
+		validProp1: 'validProp',
+		validProp2: null,
+	};
+
+  expect(mergePropsWithAvailableProps(allowedProps, props)).toEqual(expected);
+});
